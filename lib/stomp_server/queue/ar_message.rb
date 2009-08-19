f47658384df9fa724f9079b44a2198db4d1cbb66
robl <robl@monkeyhelper.com>
2009-08-19 13:35:16 +0100

    Added activerecord reconnection code to stop 'lost connection to mysql server' errors

--- lib/stomp_server/queue/ar_message.rb
+++ lib/stomp_server/queue/ar_message.rb
@@ -1,5 +1,23 @@
 require 'active_record'
 
+# Added reconnection patch
+# http://snipplr.com/view.php?codeview&id=4195
+module ActiveRecord::ConnectionAdapters
+  class MysqlAdapter
+    alias :orig_execute :execute 
+    def execute(sql,name=nil)
+      orig_execute(sql,name)
+      rescue ActiveRecord::StatementInvalid => exception
+        if LOST_CONNECTION_ERROR_MESSAGES.any? { |msg| exception.message. =~ /#{msg}/ } 
+          reconnect!
+          retry
+        else
+          raise
+        end
+    end
+  end
+end
+
 class ArMessage < ActiveRecord::Base
   serialize :frame
 end
