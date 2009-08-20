2a6cbdd3cab03f89dcdd866225b5ebb0431829ba
robl <robl@monkeyhelper.com>
2009-08-20 17:53:12 +0100

    Fixed ar reconnection

--- lib/stomp_server/queue/ar_message.rb
+++ lib/stomp_server/queue/ar_message.rb
@@ -1,22 +1,6 @@
+gem 'activerecord'
 require 'active_record'
-
-# Added reconnection patch
-# http://snipplr.com/view.php?codeview&id=4195
-module ActiveRecord::ConnectionAdapters
-  class MysqlAdapter
-    alias :orig_execute :execute 
-    def execute(sql,name=nil)
-      orig_execute(sql,name)
-      rescue ActiveRecord::StatementInvalid => exception
-        if LOST_CONNECTION_ERROR_MESSAGES.any? { |msg| exception.message. =~ /#{msg}/ } 
-          reconnect!
-          retry
-        else
-          raise
-        end
-    end
-  end
-end
+require 'stomp_server/queue/ar_reconnect'
 
 class ArMessage < ActiveRecord::Base
   serialize :frame
