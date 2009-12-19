d55f5b1955f3d1cf67b0c217e74c9028ee2ed17d
Guy M. Allard <allard.guy.m@gmail.com>
2009-12-19 16:43:46 -0500

    Fix bug with log level misspelling. Fix bug with command line options not overriding config file options.

--- lib/stomp_server.rb
+++ lib/stomp_server.rb
@@ -19,7 +19,7 @@ module StompServer
     #
     def self.set_loglevel(opts)
       @@loglevel = nil
-      case opts[:loglevel].downcase
+      case opts[:log_level].downcase
         when 'debug' then @@loglevel = Logger::DEBUG
         when 'info'  then @@loglevel = Logger::INFO
         when 'warn'  then @@loglevel = Logger::WARN
@@ -51,7 +51,7 @@ module StompServer
         :logdir => 'log',
         :configfile => 'stompserver.conf',
         :logfile => 'stompserver.log',
-        :loglevel => 'error',
+        :log_level => 'error',
         :pidfile => 'stompserver.pid',
         :checkpoint => 0
       }
@@ -60,7 +60,7 @@ module StompServer
       @@log.debug "stomp_server version: #{StompServer::VERSION}"
 
       @opts = getopts   # get the options
-      @@log.debug "Logger Level Requested: #{@opts[:loglevel].upcase}"
+      @@log.debug "Logger Level Requested: #{@opts[:log_level].upcase}"
       StompServer::LogLevelHandler.set_loglevel(@opts)
       @@log.level = StompServer::LogLevelHandler.get_loglevel()
       #
@@ -72,30 +72,41 @@ module StompServer
     end
 
     def getopts
-      copts = OptionParser.new
-      copts.on("-a", "--auth", String, "Require client authorization") {|a| @defaults[:auth] = true}
-      copts.on("-b", "--host=ADDR", String, "Change the host (default: localhost)") {|a| @defaults[:host] = a}
-      copts.on("-c", "--checkpoint=SECONDS", Integer, "Time between checkpointing the queues in seconds (default: 0)") {|c| @defaults[:checkpoint] = c}
-      copts.on("-C", "--config=CONFIGFILE", String, "Configuration File (default: stompserver.conf)") {|c| @defaults[:configfile] = c}
-      copts.on("-d", "--debug", String, "Turn on debug messages") {|d| @defaults[:debug] = true}
-      copts.on("-l", "--log_level=LEVEL", String, "Logger Level (default: ERROR") {|l| @defaults[:loglevel] = l}
-      copts.on("-p", "--port=PORT", Integer, "Change the port (default: 61613)") {|p| @defaults[:port] = p}
-      copts.on("-q", "--queuetype=QUEUETYPE", String, "Queue type (memory|dbm|activerecord|file) (default: memory)") {|q| @defaults[:queue] = q}
-      copts.on("-s", "--storage=DIR", String, "Change the storage directory (default: .stompserver, relative to working_dir)") {|s| @defaults[:storage] = s}
-      copts.on("-w", "--working_dir=DIR", String, "Change the working directory (default: current directory)") {|s| @defaults[:working_dir] = s}
+      opts_parser = OptionParser.new
+      hopts = {}
+      opts_parser.on("-a", "--auth", String, "Require client authorization") {|a| hopts[:auth] = true}
+      opts_parser.on("-b", "--host=ADDR", String, "Change the host (default: localhost)") {|a| hopts[:host] = a}
+      opts_parser.on("-c", "--checkpoint=SECONDS", Integer, "Time between checkpointing the queues in seconds (default: 0)") {|c| hopts[:checkpoint] = c}
+      opts_parser.on("-C", "--config=CONFIGFILE", String, "Configuration File (default: stompserver.conf)") {|c| hopts[:configfile] = c}
+      opts_parser.on("-d", "--debug", String, "Turn on debug messages") {|d| hopts[:debug] = true}
+      opts_parser.on("-l", "--log_level=LEVEL", String, "Logger Level (default: ERROR") {|l| hopts[:log_level] = l}
+      opts_parser.on("-p", "--port=PORT", Integer, "Change the port (default: 61613)") {|p| hopts[:port] = p}
+      opts_parser.on("-q", "--queuetype=QUEUETYPE", String, "Queue type (memory|dbm|activerecord|file) (default: memory)") {|q| hopts[:queue] = q}
+      opts_parser.on("-s", "--storage=DIR", String, "Change the storage directory (default: .stompserver, relative to working_dir)") {|s| hopts[:storage] = s}
+      opts_parser.on("-w", "--working_dir=DIR", String, "Change the working directory (default: current directory)") {|s| hopts[:working_dir] = s}
       #
-      copts.on("-h", "--help", "Show this message") do
-        puts copts
+      opts_parser.on("-h", "--help", "Show this message") do
+        puts opts_parser
         exit
       end
 
-      copts.parse(ARGV)
+      opts_parser.parse(ARGV)
 
-      if File.exists?(@defaults[:configfile])
-        opts = @defaults.merge(YAML.load_file(@defaults[:configfile]))
+      loaded_opts = {}
+      if hopts[:configfile]
+        @@log.debug("Config file is: #{hopts[:configfile]}")
+        loaded_opts = YAML.load_file(hopts[:configfile])
+      elsif File.exists?(@defaults[:configfile])
+        @@log.debug("Config file is: #{@defaults[:configfile]}")
+        loaded_opts = YAML.load_file(@defaults[:configfile])
       else
-        opts = @defaults
+        @@log.warn("Config file not found")
       end
+
+      opts = {}
+      opts = opts.merge(@defaults)
+      opts = opts.merge(loaded_opts)
+      opts = opts.merge(hopts)
 
       opts[:etcdir] = File.join(opts[:working_dir],'etc')
       opts[:storage] = File.join(opts[:working_dir],opts[:storage])
