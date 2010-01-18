9b8057c992b32aefed84dea51b56e0264274d682
gmallard <allard.guy.m@gmail.com>
2010-01-18 09:51:24 -0500

    Format comments and supply indentation.

--- lib/stomp_server/stomp_id.rb
+++ lib/stomp_server/stomp_id.rb
@@ -1,21 +1,32 @@
-
-# This class is instantiated in all the queue storage classes, plus the queue manager (for the statistic messages).  It generates a unique
-# id for each message.  The caller passes an additional identifier that is appended message-id, which is usually the id of the frame and is
+#
+# This class is instantiated in all the queue storage classes, plus the 
+# queue manager (for the statistic messages).  It generates a unique
+# id for each message.  The caller passes an additional identifier that is 
+# appended message-id, which is usually the id of the frame and is
 # different for each storage class.
-
+#
 require 'socket'
 require 'resolv-replace'
-
+#
 module StompServer
-class StompId
-
-  def initialize
-    @host = Socket.gethostname.to_s
+  #
+  class StompId
+    #
+    # Get current hostname
+    #
+    def initialize
+      @host = Socket.gethostname.to_s
+    end
+    #
+    # Generate a 'unique' id based on:
+    #
+    # * Host name
+    # * Current time
+    # * Called supplied id
+    #
+    def [](id)
+      msgid = sprintf("%.6f",Time.now.to_f).to_s.sub('.','-')
+      msgid = @host + '-' + msgid + '-' + id.to_s
+    end
   end
-
-  def [](id)
-    msgid = sprintf("%.6f",Time.now.to_f).to_s.sub('.','-')
-    msgid = @host + '-' + msgid + '-' + id.to_s
-  end
-end
 end
