b457e475418686dfc0da711499f55c005a359bde
gmallard <allard.guy.m@gmail.com>
2010-06-17 23:37:51 -0400

    Move monitor to separate file.

---
+++ lib/stomp_server_ng/qmonitor.rb
@@ -0,0 +1,54 @@
+#
+# = Queue Monitor
+#
+module StompServer
+class QueueMonitor
+  #
+  # initialize
+  #
+  def initialize(qstore,queues)
+    @qstore = qstore
+    @queues = queues
+    @stompid = StompServer::StompId.new
+    #
+    @@log = Logger.new(STDOUT)
+    @@log.level = StompServer::LogHelper.get_loglevel()
+    @@log.debug("QueueMonitor initialize comletes")
+    #
+  end
+  #
+  # start
+  #
+  def start
+    count =0
+    EventMachine::add_periodic_timer 5, proc {count+=1; monitor(count) }
+  end
+  #
+  # monitor
+  #
+  def monitor(count)
+    return unless (@qstore.methods.include?(:monitor) | @qstore.methods.include?('monitor'))
+    users = @queues['/queue/monitor']
+    return if users.size == 0
+    stats = @qstore.monitor
+    return if stats.size == 0
+    body = ''
+    #
+    stats.each do |queue,qstats|
+      body << "Queue: #{queue}\n"
+      qstats.each {|stat,value| body << "#{stat}: #{value}\n"}
+      body << "\n"
+    end
+    #
+    headers = {
+      'message-id' => @stompid[count],
+      'destination' => '/queue/monitor',
+      'content-length' => body.size.to_s
+    }
+    #
+    frame = StompServer::StompFrame.new('MESSAGE', headers, body)
+    users.each {|user| user.connection.stomp_send_data(frame)}
+  end
+end # of class QueueMonitor
+end # of module StompServer
+
