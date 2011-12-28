6cdec58d65952e6c683b97115f3357c0d81f3fb8
gmallard <allard.guy.m@gmail.com>
2011-12-28 08:43:09 -0500

    Tweak test server run for readability.

--- test/devserver/runserver.sh
+++ test/devserver/runserver.sh
@@ -4,8 +4,8 @@ set -x
 #
 PH=$(pwd)
 echo "Project Home: $PH"
-mtype=${QMEM:-.file}
-CONFIG=$PH/test/devserver/stompserver_ng${mtype}.conf
+mtype=${QMEM:-file}
+CONFIG=$PH/test/devserver/stompserver_ng.${mtype}.conf
 echo "Using Config: $CONFIG"
 #
 FLAGS="--log_level=debug --config=$CONFIG $*"
